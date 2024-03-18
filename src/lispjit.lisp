c7261e68eb7736fd3a623f65bb99f52906fd1bc5
Yuito Murase <yuito@acupof.coffee>
2024-03-18 20:17:22 +0900

    Support function and variable

--- src/lispjit.lisp
+++ src/lispjit.lisp
@@ -1,34 +1,61 @@
 (defpackage :lispjit
   (:use :common-lisp)
-  (:export :interpret))
+  (:export :interpret :fn))
 
 (in-package :lispjit)
 
-(defun interpret% (code)
+(defun lookup-var (env var)
+  (if (null env)
+      nil
+      (let ((res (assoc var (car env))))
+        (if res
+            (cdr res)
+            (lookup-var (cdr env) var)))))
+
+(defun extend-env (vars args env)
+  (cons (mapcar #'cons vars args) env))
+
+(defstruct closure env args body)
+
+(defun interpret% (code env)
   (cond
     ((integerp code) code)
     ((or (eq code t) (eq code nil)) code)
+    ((symbolp code) (lookup-var env code))
     ((consp code)
-     (destructuring-bind (op . args) code
-       (case op
-         (+ (destructuring-bind (a b) args
-               (let* ((ea (interpret% a))
-                      (eb (interpret% b)))
-                 (+ ea eb))))
-         (* (destructuring-bind (a b) args
-               (let* ((ea (interpret% a))
-                      (eb (interpret% b)))
-                 (* ea eb))))
-         (>= (destructuring-bind (a b) args
-                (let* ((ea (interpret% a))
-                       (eb (interpret% b)))
-                  (>= ea eb))))
-         (not (destructuring-bind (a) args
-                (not (interpret% a))))
-         (if (destructuring-bind (condition then else) args
-               (let ((ec (interpret% condition)))
+     (destructuring-bind (head . tail) code
+       (case head
+         (fn (destructuring-bind (args body) tail
+               (make-closure :env env :args args :body body)))
+         ;; primitive functions
+         (+ (destructuring-bind (a b) tail
+              (let* ((ea (interpret% a env))
+                     (eb (interpret% b env)))
+                (+ ea eb))))
+         (* (destructuring-bind (a b) tail
+              (let* ((ea (interpret% a env))
+                     (eb (interpret% b env)))
+                (* ea eb))))
+         (>= (destructuring-bind (a b) tail
+               (let* ((ea (interpret% a env))
+                      (eb (interpret% b env)))
+                 (>= ea eb))))
+         (not (destructuring-bind (a) tail
+                (not (interpret% a env))))
+         ;; primitive syntaxc
+         (if (destructuring-bind (condition then else) tail
+               (let ((ec (interpret% condition env)))
                  (if ec
-                     (interpret% then)
-                     (interpret% else))))))))))
+                     (interpret% then env)
+                     (interpret% else env)))))
+         (t (let* ((ehead (interpret% head env))
+                   (eargs (interpret%* tail env)))
+              (cond
+                ((closure-p ehead)
+                 (let* ((newenv (extend-env (closure-args ehead) eargs (closure-env ehead))))
+                   (interpret% (closure-body ehead) newenv)))))))))))
 
-(defun interpret (code) (interpret% code))
+(defun interpret%* (code-list env)
+  (mapcar #'(lambda (code) (interpret% code env)) code-list))
+
+(defun interpret (code) (interpret% code nil))
--- t/lispjit.lisp
+++ t/lispjit.lisp
@@ -22,6 +22,13 @@
   (evals-to 1 (if t 1 0))
   (evals-to 0 (if nil 1 0)))
 
+(test function
+  (evals-to 1 ((fn (x) 1) 2))
+  (evals-to 2 ((fn (x) x) 2))
+  (evals-to 30 ((fn (x y) (+ x y)) 10 20))
+  (evals-to 40 ((fn (f x) (f x)) (fn (x) (* x 2)) 20))
+  (evals-to 40 (((fn (x) (fn (y) (+ x y))) 10) 30)))
+
 (test combined-expr
   (evals-to 19 (+ (+ -10 11) (* 3 6)))
   (evals-to 10 (+ 2 (if (>= 6 (+ 2 3)) (* 2 4) -199))))
